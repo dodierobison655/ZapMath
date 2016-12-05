7c4358ce9bcc15d61b54535efb0b1a370757a8e2
Deral Robison <dodierobison655@gmail.com>
2016-12-05 15:01:00 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.getElementById('myCanvas');
-if(canvas.getContext){
-    var ctx = canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.arc(350,350,25,0,Math.PI*2); // Outer circle
-    ctx.stroke();
-  }
